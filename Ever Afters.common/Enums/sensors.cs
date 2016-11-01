35af58a87dac7f1eb53d2b7f7c846bc86b1d3677
Esteban Denis <esteban.denis.ed@gmail.com>
2016-11-01 17:32:27 +0100

    Made Video & Tag objects
    Video And Tag objects made, complete with static functions, CTOR's,
    fields and enough comments.

--- Ever Afters.common/Enums/sensors.cs
+++ Ever Afters.common/Enums/sensors.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace Ever_Afters.common.Enums
 {
-    public enum sensors
+    public enum Sensors
     {
+        //We chose left -> right notation in order to keep clear of the reading order of the sensors, no matter their physical or numerical location.
         NFC_LEFT, NFC_LEFT_MIDDLE, NFC_MIDDLE, NFC_RIGHT_MIDDLE, NFC_RIGHT
     }
 }
---
+++ Ever Afters.common/Models/Tag.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ever_Afters.common.Models
+{
+    public class Tag
+    {
+        //General Database Fields
+        public int id { get; set; }
+        public String name { get; set; }
+
+        /// <summary>
+        /// This is an empty constructor meant for data binding at boot time.
+        /// In code this constructor should be avoided.
+        /// </summary>
+        public Tag()
+        {
+
+        }
+
+        /// <summary>
+        /// This object represents the NFC tag in database.
+        /// This takes only a name, ideal for the creation of a new tag.
+        /// </summary>
+        /// <param name="TagName">The TAG name written on the NFC label</param>
+        public Tag(String TagName)
+        {
+            this.name = TagName;
+        }
+
+        /// <summary>
+        /// This object represents the NFC tag in database
+        /// </summary>
+        /// <param name="id">The Database Identifier</param>
+        /// <param name="TagName">The TAG name written on the NFC label</param>
+        public Tag(int id, String TagName)
+        {
+            this.id = id;
+            this.name = TagName;
+        }
+
+        //Static Functions
+        public static bool tagExists(String TagName)
+        {
+            //Check if a tag exists with given name.
+            return true;
+        }
+
+        public static bool isBound(String TagName)
+        {
+            //Check if a tag with given name is bound to a video.
+            return true;
+        }
+
+        //Overrides
+        public override string ToString()
+        {
+            return this.name;
+        }
+    }
+}
