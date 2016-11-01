fdec8652b3c562a526a35ef035038b8cb6ce5428
Esteban Denis <esteban.denis.ed@gmail.com>
2016-11-01 17:00:40 +0100

    Enumerations Made
    Made the enumerations for the DAL and Logical Layer.
    Named them from left to right.

---
+++ Ever Afters.common/Enums/sensors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ever_Afters.common.Enums
+{
+    public enum sensors
+    {
+        NFC_LEFT, NFC_LEFT_MIDDLE, NFC_MIDDLE, NFC_RIGHT_MIDDLE, NFC_RIGHT
+    }
+}
