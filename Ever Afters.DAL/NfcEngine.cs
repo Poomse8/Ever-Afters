c827b0cb60d4bf77afc7bf26c1113f8e4de86c55
Poomse8 <Nicolas.delanghe@student.howest.be>
2016-11-14 18:54:54 +0100

    12 okt: 14.30 - 22.00

---
+++ Ever Afters.DAL/NfcEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ever_Afters.DAL
+{
+    public class NfcEngine
+    {
+        private static NfcEngine instance = new NfcEngine();
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static NfcEngine() { }
+
+        private NfcEngine(){ }
+
+        public static NfcEngine Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
--- Ever Afters/MainPage.xaml.cs
+++ Ever Afters/MainPage.xaml.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Chat;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Media.Core;
 using Windows.Storage;
 using Windows.Storage.FileProperties;
+using Windows.System;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -21,6 +27,7 @@ using Ever_Afters.common.Core;
 using Ever_Afters.common.Enums;
 using Ever_Afters.common.Listeners;
 using Ever_Afters.common.Models;
+using Ever_Afters.DAL;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -34,10 +41,17 @@ namespace Ever_Afters
         private InputChangedListener il;
         private bool toggle = false;
 
+        //Nico's vars
+        private bool _overrideKeyboard = true;
+        private NfcEngine _nfcEngine = NfcEngine.Instance;
+        private string _currentRead = String.Empty;
+        private long prevCharMilliseconds;
+
         public MainPage()
         {
             this.InitializeComponent();
             InitialiseEngine();
+            InitializeNfc();
         }
 
         public void InitialiseEngine()
@@ -109,5 +123,68 @@ namespace Ever_Afters
                 Engine.CurrentEngine.VideoStarted();
             }
         }
+
+        #region Buttons (Nico)
+
+        private void btnReader_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateBtnColors(sender);
+
+        }
+
+        private void UpdateBtnColors(object sender)
+        {
+            btnReader1.Background = new SolidColorBrush(Color.FromArgb(51, 00, 116, 255));
+            btnReader2.Background = new SolidColorBrush(Color.FromArgb(51, 00, 116, 255));
+            btnReader3.Background = new SolidColorBrush(Color.FromArgb(51, 00, 116, 255));
+            btnReader4.Background = new SolidColorBrush(Color.FromArgb(51, 00, 116, 255));
+            btnReader5.Background = new SolidColorBrush(Color.FromArgb(51, 00, 116, 255));
+
+            var button = sender as Button;
+            if (button != null) button.Background = new SolidColorBrush(Color.FromArgb(51, 00, 255, 139));
+        }
+
+        private void UserInputFocusGained(object sender, RoutedEventArgs e)
+        {
+            _overrideKeyboard = false;
+        }
+
+        private void UserInputFocusLost(object sender, RoutedEventArgs e)
+        {
+            txtNfcListener.Focus(FocusState.Keyboard);
+            _overrideKeyboard = true;
+        }
+        
+        private void InitializeNfc()
+        {
+            txtNfcListener.LostFocus += TxtNfcListener_LostFocus;
+        }
+
+        private void TxtNfcListener_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(_overrideKeyboard) txtNfcListener.Focus(FocusState.Keyboard);
+        }
+
+        #endregion
+
+        private void Page_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (_overrideKeyboard)
+            {
+                long currentCharMilliseconds = DateTime.Now.Millisecond;
+                if(currentCharMilliseconds - prevCharMilliseconds > 500)
+                {
+                    Debug.WriteLine(txtNfcListener.Text);
+                }
+
+                prevCharMilliseconds = currentCharMilliseconds;
+            }
+        }
+
+        private void InitialiseNfcRead(char letter)
+        {
+            Debug.WriteLine("char ok:" + letter);
+        }
+
     }
 }
