1e808e863aadee47c1e8ab55d35159e7e2dcbd68
Esteban Denis <esteban.denis.ed@gmail.com>
2016-12-26 13:11:40 +0100

    Administrator Windows Version 1.1
    The Administrator Windows - Version 1.1
    Known Bugs: Deleting a video with a bound tag!

--- Ever Afters.admin/MainPage.xaml.cs
+++ Ever Afters.admin/MainPage.xaml.cs
@@ -2,22 +2,12 @@
 using Ever_Afters.common.Models;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.System.Threading;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using Ever_Afters.common.DatabaseLayer;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
